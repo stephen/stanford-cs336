6b3072933bc6d0a8ce50a650023c0b897dd88bc3
Stephen Wan <stephen@stephenwan.com>
2025-06-03 13:00:10 -0400

    tokenizer: keep track of counts through cache
    This takes the valid.txt dataset from 5s to 2.5s. At this point, the profiler doesn't show any obvious bad things happening in the loop.

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -21,6 +21,9 @@ _Refs = dict[_Sequence, int]
 # _TokenPair is a pair of tokens.
 _TokenPair = tuple[_Token, _Token]
 
+class _TokenPairCounts(TypedDict):
+    count: int
+    refs: _Refs
 
 def bpe_tokenize(text: bytes, num_merges: int) -> list[bytes]:
     vocab = list([b.to_bytes() for b in range(0, 256)])
@@ -46,45 +49,48 @@ def bpe_tokenize(text: bytes, num_merges: int) -> list[bytes]:
 
         corpus[key] += 1
 
-    # pairs is the state of all token pairs in the corpus, pointing to where they happen in the corpus.
-    pairs: dict[_TokenPair, _Refs] = {}
+    # pairs is the state of all token pairs in the corpus, pointing to where they happen in the corpus and the total count the pair happens.
+    pairs: dict[_TokenPair, _TokenPairCounts] = {}
 
     # Go through each sequence that needs to be checked. At the start, this is the entire corpus but
     # later this is only sequences that need to be rechecked.
     corpus_to_check = corpus.keys()
     for _ in range(num_merges):
         for pretoken in corpus_to_check:
+            count = corpus[pretoken]
             for i, pair in enumerate(zip(pretoken, pretoken[1:])):
                 if pair not in pairs:
-                    pairs[pair] = {}
-                pairs[pair][pretoken] = i
+                    pairs[pair] = {'count': 0, 'refs': {}}
+                pairs[pair]['refs'][pretoken] = i
+                pairs[pair]['count'] += count
 
         # Figure out the max pair.
         # XXX: We could maybe keep track of this state during the loop.
-        max_pair = max(pairs, key=lambda k: (sum(corpus[k] for k in pairs[k].keys()), k))
+        max_pair = max(pairs, key=lambda k: (pairs[k]['count'], k))
         vocab.append(b''.join(max_pair))
 
         # Invalidate pairs only at the spots where the sequence changed because the underlying tokens
         # changed due to max_pair.
         corpus_to_check = set()
-        pairs_to_invalidate: set[tuple[_TokenPair, _Sequence]] = set()
-        for old_seq, i in pairs[max_pair].items():
+        pairs_to_invalidate: set[tuple[_TokenPair, _Sequence, _Sequence]] = set()
+        for old_seq, i in pairs[max_pair]['refs'].items():
             new_seq = old_seq[:i] + (max_pair[0] + max_pair[1],) + old_seq[i+2:]
 
             # For all pairs of the old sequence, remove their reference.
             for i, neighbor_pair in enumerate(zip(old_seq, old_seq[1:])):
-                pairs_to_invalidate.add((neighbor_pair, old_seq))
+                pairs_to_invalidate.add((neighbor_pair, old_seq, new_seq))
 
             corpus_to_check.add(new_seq)
             corpus[new_seq] = corpus[old_seq]
             del corpus[old_seq]
 
-        for (neighbor_pair, old_seq) in pairs_to_invalidate:
-            del pairs[neighbor_pair][old_seq]
+        for (neighbor_pair, old_seq, new_seq) in pairs_to_invalidate:
+            del pairs[neighbor_pair]['refs'][old_seq]
+            pairs[neighbor_pair]['count'] -= corpus[new_seq]
 
     return vocab
 
-with open("./data/TinyStoriesV2-GPT4-valid.txt", "rb") as f:
+with open("./data/TinyStoriesV2-GPT4-train.txt", "rb") as f:
     content = f.read()
     start = time.perf_counter()
     bpe_tokenize(content, 1000)
