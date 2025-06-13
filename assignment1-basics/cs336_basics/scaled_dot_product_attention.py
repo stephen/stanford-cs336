3eaca7b12898b7bd4789f4918f68c41eccd3d87f
Stephen Wan <stephen@stephenwan.com>
2025-06-12 22:58:25 -0400

    attention: fix mask typing

--- assignment1-basics/cs336_basics/scaled_dot_product_attention.py
+++ assignment1-basics/cs336_basics/scaled_dot_product_attention.py
@@ -2,21 +2,23 @@ import math
 import einops
 from jaxtyping import Float
 import torch as t
+from typing import Optional
 
 from cs336_basics.softmax import softmax
 
 def scaled_dot_product_attention(
-    q: Float[t.Tensor, "b n d"],
-    k: Float[t.Tensor, "b m d"],
-    v: Float[t.Tensor, "b m d"],
-    mask: Float[t.Tensor, "b n m"],
-) -> Float[t.Tensor, "d d"]:
+    q: Float[t.Tensor, "... n d"],
+    k: Float[t.Tensor, "... m d"],
+    v: Float[t.Tensor, "... m d"],
+    mask: Optional[Float[t.Tensor, "... n m"]],
+) -> Float[t.Tensor, "n d"]:
     q_k = einops.einsum(q, k, "... n d, ... m d -> ... n m")
     d = k.shape[-1]
 
     # Note that mask = False means "ignore". It's more like an "allow mask".
-    pre_softmax_mask = t.where(mask == False, -math.inf, 0)
-
-    z = softmax((q_k / math.sqrt(d)) + pre_softmax_mask, dim=-1)
+    z = (q_k / math.sqrt(d))
+    if mask is not None:
+        z += t.where(mask == False, -math.inf, 0)
+    z = softmax(z, dim=-1)
 
     return einops.einsum(z, v, "... n m, ... m d -> ... n d")
