fa7a1d073e048731dca2d43b89e335dec5e753f9
Stephen Wan <stephen@stephenwan.com>
2025-06-18 10:14:38 -0400

    crossentropy: fix dimension issue and use logsumexp

--- assignment1-basics/cs336_basics/cross_entropy_loss.py
+++ assignment1-basics/cs336_basics/cross_entropy_loss.py
@@ -3,10 +3,13 @@ import torch as t
 
 def cross_entropy(logits: Float[t.Tensor, "b d"], target: Int[t.Tensor, "b"]) -> t.Tensor:
     log_probs = log_softmax(logits, dim=-1)
-    # return -log_probs.gather(dim=-1, index=target.unsqueeze(1)).mean().squeeze()
-    return -log_probs[t.arange(logits.size(0)), target].mean()
+    return -log_probs.gather(dim=-1, index=target.unsqueeze(1)).squeeze(-1).mean()
 
 def log_softmax(logits: Float[t.Tensor, "b d"], dim: int):
     m = logits.max(dim=dim, keepdim=True).values
     v = logits - m
-    return v - t.log(v.exp().sum(dim=dim, keepdim=True))
+    return v - logsumexp(v, dim)
+
+def logsumexp(x: t.Tensor, dim: int) -> t.Tensor:
+    m = x.max(dim=dim, keepdim=True).values
+    return m + t.log((x - m).exp().sum(dim=dim, keepdim=True))
