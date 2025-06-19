3c5a5b6a716b7dce249b4e42aaa0b632c57b700c
Stephen Wan <stephen@stephenwan.com>
2025-06-19 02:12:37 -0400

    crossentropy: fix buggy dimensional indexing
    This took me forever to figure out but before this bug I was
    indexing into gibberish and the loss would get stuck at ~5.8

--- assignment1-basics/cs336_basics/cross_entropy_loss.py
+++ assignment1-basics/cs336_basics/cross_entropy_loss.py
@@ -3,7 +3,7 @@ import torch as t
 
 def cross_entropy(logits: Float[t.Tensor, "b d"], target: Int[t.Tensor, "b"]) -> t.Tensor:
     log_probs = log_softmax(logits, dim=-1)
-    return -log_probs.gather(dim=-1, index=target.unsqueeze(1)).squeeze(-1).mean()
+    return -log_probs.gather(dim=-1, index=target.unsqueeze(-1)).squeeze(-1).mean()
 
 def log_softmax(logits: Float[t.Tensor, "b d"], dim: int):
     m = logits.max(dim=dim, keepdim=True).values
